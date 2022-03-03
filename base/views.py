b0b0672263b1272436664dbba02ed87698f9856f
Jackk-Doe <dodo-stv@hotmail.com>
2022-03-03 21:59:17 +1300

    Login & Logout system
    - Implemented Login & Logout function
    - Set restrictions for accessing CRUD

--- base/views.py
+++ base/views.py
@@ -2,30 +2,41 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin   #User login required for inherited classes
 from django.urls import reverse_lazy
 from .models import Task
 
-class TaskList(ListView):   #Auto look for html (template) file with same class name (task_list.html)
+# Logout function in urls.py
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True     #Prevent User to re-login
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')    #Once logined, redirect to this url
+
+class TaskList(LoginRequiredMixin, ListView):   #Auto look for html (template) file with same class name (task_list.html)
     model = Task
     context_object_name = 'tasks'   #How class's obj is called in html
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'    #Rewrite Auto template file seeking function
 
-class TaskCreate(CreateView):   #The CreateView auto create form field inside it
+class TaskCreate(LoginRequiredMixin, CreateView):   #The CreateView auto create form field inside it
     model = Task
     fields = '__all__'      #Use all fields in assigned [model]
     success_url = reverse_lazy('tasks') #If all went successfully, go to 'tasks' in urlpatterns
 
-class TaskUpdate(UpdateView):   #Auto fill form field, with given fields var
+class TaskUpdate(LoginRequiredMixin, UpdateView):   #Auto fill form field, with given fields var
     model = Task 
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
-class DeleteView(DeleteView):
+class DeleteView(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
-    
