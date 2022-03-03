b3c2c74e4368239fbb11731cfbc5b7c83745decb
Jackk-Doe <dodo-stv@hotmail.com>
2022-03-03 17:33:29 +1300

    Model, Templates, View functions
    - Created Model
    - Created Templated
    - Class-based view functions
    - CRUD functions

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    # If the User is deleted this Task will also be delete (models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=True)
+    created = models.DateField(auto_now_add=True)   #Set to creating date
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']     #Order data by [complete] var
--- base/views.py
+++ base/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
-# Create your views here.
+class TaskList(ListView):   #Auto look for html (template) file with same class name (task_list.html)
+    model = Task
+    context_object_name = 'tasks'   #How class's obj is called in html
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'    #Rewrite Auto template file seeking function
+
+class TaskCreate(CreateView):   #The CreateView auto create form field inside it
+    model = Task
+    fields = '__all__'      #Use all fields in assigned [model]
+    success_url = reverse_lazy('tasks') #If all went successfully, go to 'tasks' in urlpatterns
+
+class TaskUpdate(UpdateView):   #Auto fill form field, with given fields var
+    model = Task 
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    
